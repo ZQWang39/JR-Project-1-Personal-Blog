a96c77536661411ac5f4a5308d542da318dbb0aa
Ziqi Wang <ziqi.wang39@gmail.com>
2021-08-15 22:44:25 +1000

    Update submit form

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ import {displayJob} from './display-job.js'
 
 navSlide();
 displayJob();
+
+
+const contactForm = document.querySelector('.contact__form');
+let name = document.getElementById('name');
+let button = document.querySelector('.button');
+console.log(button)
+console.log(name)
+ contactForm.addEventListener('submit',(e)=>{
+     e.preventDefault();
+    console.log(name.value)
+ })
